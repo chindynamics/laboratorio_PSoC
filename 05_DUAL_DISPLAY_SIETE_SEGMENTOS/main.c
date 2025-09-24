46acb78ad837b8866e33ce8b05f0e6338325cb23
chin <chindynamicss@gmail.com>
2025-09-23 19:09:26 -0600

    Add files via upload

---
+++ 05_DUAL_DISPLAY_SIETE_SEGMENTOS/main.c
@@ -0,0 +1,79 @@
+    #include "DDisplay.h"
+    #include "project.h"
+    #include <cytypes.h>
+
+    const uint16 pattern[] = {
+        
+        // PATRON DE HO
+        ( (DDISPLAY_SEGMENT_F_L << 8 | DDISPLAY_SEGMENT_E_L << 8 | DDISPLAY_SEGMENT_B_L << 8 | DDISPLAY_SEGMENT_C_L << 8 | DDISPLAY_SEGMENT_G_L << 8) |
+          (DDISPLAY_SEGMENT_A_R | DDISPLAY_SEGMENT_F_R | DDISPLAY_SEGMENT_E_R | DDISPLAY_SEGMENT_D_R | DDISPLAY_SEGMENT_C_R | DDISPLAY_SEGMENT_B_R)
+        ),
+        
+        // PATRON LA
+        ( (DDISPLAY_SEGMENT_F_L << 8 | DDISPLAY_SEGMENT_E_L << 8 | DDISPLAY_SEGMENT_D_L << 8) |
+          (DDISPLAY_SEGMENT_A_R | DDISPLAY_SEGMENT_F_R | DDISPLAY_SEGMENT_E_R | DDISPLAY_SEGMENT_C_R | DDISPLAY_SEGMENT_B_R | DDISPLAY_SEGMENT_G_R)
+        ),
+        
+        // PATRON DE __
+        ( (DDISPLAY_SEGMENT_D_L << 8 ) |
+          (DDISPLAY_SEGMENT_D_R)
+        ),
+        
+    };  
+
+    int main(void)
+    {
+        CyGlobalIntEnable; 
+        
+        DDisplay_Start();
+        
+        uint8 size = sizeof(pattern) / sizeof(pattern[0]);
+        
+        // PRUEBA 1 MODO DECIMAL (00 A 99)
+        
+        DDisplay_SetMode(DDISPLAY_MODE_DECIMAL);
+        for (uint8 i = 0; i <= 99; i++)
+        {
+            DDisplay_SetValue(i);
+            CyDelay(50);
+        }
+        
+        // --- PRUEBA 2: INCREMENTAR y DECREMENTAR ---
+        DDisplay_SetValue(0); // Empezar en 0
+        CyDelay(2000);
+        for (uint8 i = 0; i < 15; i++)
+        {
+            DDisplay_IncrementValue();
+            CyDelay(200);
+        }
+        CyDelay(1000);
+        for (uint8 i = 0; i < 5; i++)
+        {
+            DDisplay_DecrementValue();
+            CyDelay(200);
+        }
+        CyDelay(2000);
+        
+        // --- PRUEBA 3: MODO HEXADECIMAL (00 a FF) ---
+        DDisplay_SetMode(DDISPLAY_MODE_HEXADECIMAL);
+        DDisplay_SetValue(240); 
+        CyDelay(1000);
+        DDisplay_SetValue(50); 
+        CyDelay(1000);
+        DDisplay_SetValue(89); 
+        CyDelay(1000);
+        
+        // --- PRUEBA 4: MODO DE PATRONES ---
+        DDisplay_SetMode(DDISPLAY_MODE_PATTERNS);
+        DDisplay_SetPatternsArray(pattern, size);
+        DDisplay_ShowPattern(0);
+        
+        CyDelay(2000);
+        
+        
+        for(;;)
+        {
+            DDisplay_NextPattern();
+            CyDelay(1000);
+        }
+    }
